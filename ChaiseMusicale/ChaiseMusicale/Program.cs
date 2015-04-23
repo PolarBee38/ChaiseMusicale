ed85608db34c99a17770a1a20164684d4ba6b5cb
SchwartzMarlene <mcc.schwartz@free.fr>
2015-04-23 15:51:37 +0200

    Hello world

--- ChaiseMusicale/ChaiseMusicale/Program.cs
+++ ChaiseMusicale/ChaiseMusicale/Program.cs
@@ -10,6 +10,8 @@ namespace ChaiseMusicale
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!!");
+            Console.ReadKey();
         }
     }
 }
