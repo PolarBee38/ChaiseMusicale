623e4396378b68876035cb47dc41cbfe58bd318f
SchwartzMarlene <mcc.schwartz@free.fr>
2015-05-04 13:40:08 +0200

    version propre

--- ChaiseMusicale/ChaiseMusicale/Properties/AssemblyInfo.cs
+++ ChaiseMusicale/ChaiseMusicale/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("5dca2122-aeae-48f2-aa8d-b390c1fe30d2")]
+[assembly: Guid("7165fe1e-43ff-45ef-9b98-3952ec253fbf")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
--- ChaiseMusicale/ChaiseMusicale/TableConfiguration.cs
+++ ChaiseMusicale/ChaiseMusicale/TableConfiguration.cs
@@ -2,27 +2,28 @@
 
 namespace ChaiseMusicale
 {
-	public class TableConfiguration
-	{
-		private string _name; 
-		//private List<table> table; 
-		public TableConfiguration (string name, List<Table> table)
-		{
-			_name = name;  
-			table = new List<Table> (table); 
-		}
+    public class TableConfiguration
+    {
+        private string _name;
+        //private List<table> table; 
+        public TableConfiguration(string name, List<Table> table)
+        {
+            _name = name;
+            table = new List<Table>(table);
+        }
 
-		public string name
-		{
-			get {return _name;}
-			//set {_name = value;}
-		}
+        public string name
+        {
+            get { return _name; }
+            //set {_name = value;}
+        }
 
-		public void updateStatus(Table table)
-		{
-			
-		}
-			
+        public void updateStatus(Table table)
+        {
 
+        }
+
+
+    }
 }
 
